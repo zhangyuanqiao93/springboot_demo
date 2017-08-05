7751b844d87fae49e7c4e5e814f2a993485ef6d2
Bridge <zhangyuanqiao0912@163.com>
2017-08-05 13:26:25 +0800

    IntelliJ IDEA 修改新建文件或者类时,自动生成的注释的user的默认值，以及如何自定义

---
+++ src/main/java/com/imooc/demo/common/Demo.java
@@ -0,0 +1,9 @@
+package com.imooc.demo.common;
+
+/**
+ * Create By Bridge On 2017/8/5
+ * Function: setting-->Edit--->File and Code Templates--->includes
+ * Description: 创建一个类等操作时，自动的创建HEADER部分的个人信息。
+ */
+public class Demo {
+}
