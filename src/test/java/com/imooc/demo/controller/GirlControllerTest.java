f5ef180f0cf2e898b0735521043d7b0377715cbf
Bridge <zhangyuanqiao0912@163.com>
2017-08-06 11:59:34 +0800

    Spring Boot 的Web进阶——完结。总结，主要学习的知识点：@Valid注解进行表单验证；AOP面向切面编程，JUnit单元测试等基本的Spring Boot 应用。

--- src/test/java/com/imooc/demo/controller/GirlControllerTest.java
+++ src/test/java/com/imooc/demo/controller/GirlControllerTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 /**
  * Create By Bridge On 2017/8/5
  * Function:  mvn打包工具可以直接执行所有的单元测试。
- * Description:
+ * Description: 测试不同的方法，可以将get换成post，delete，put等，测试方法类似。
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -24,9 +24,15 @@ public class GirlControllerTest {
     private MockMvc mockMvc;
     @Test
     public void girlEntityList() throws Exception {
+
+        //实际返回的字符串
+        //String str = new String({"id":1,"cupSize":"E","age":18,"money":22.0},{"id":2,"cupSize":"E","age":18,"money":22.0},{"id":3,"cupSize":"E","age":18,"money":22.0},{"id":4,"cupSize":"E","age":18,"money":22.0},{"id":5,"cupSize":"E","age":18,"money":22.0},{"id":6,"cupSize":"E","age":18,"money":22.0},{"id":7,"cupSize":"E","age":18,"money":22.0},{"id":8,"cupSize":"E","age":18,"money":22.0},{"id":9,"cupSize":"E","age":18,"money":22.0},{"id":10,"cupSize":"E","age":22,"money":22.0},{"id":11,"cupSize":"D","age":18,"money":2.0},{"id":12,"cupSize":"D","age":18,"money":2.0},{"id":13,"cupSize":"D","age":22,"money":2.0},{"id":14,"cupSize":"D","age":22,"money":2.0},{"id":15,"cupSize":"D","age":22,"money":2.0},{"id":16,"cupSize":"D","age":22,"money":2.0},{"id":17,"cupSize":"D","age":22,"money":2.0},{"id":18,"cupSize":"D","age":22,"money":2.0},{"id":19,"cupSize":"D","age":22,"money":2.0},{"id":20,"cupSize":"D","age":22,"money":2.0},{"id":21,"cupSize":"D","age":22,"money":2.0},{"id":22,"cupSize":"D","age":22,"money":2.0},{"id":23,"cupSize":"D","age":22,"money":2.0},{"id":24,"cupSize":"D","age":22,"money":2.0},{"id":25,"cupSize":"D","age":22,"money":2.0},{"id":26,"cupSize":"D","age":22,"money":2.0},{"id":27,"cupSize":"D","age":22,"money":2.0},{"id":28,"cupSize":"D","age":22,"money":2.0},{"id":29,"cupSize":"D","age":22,"money":2.0},{"id":30,"cupSize":"D","age":22,"money":2.0},{"id":31,"cupSize":"D","age":22,"money":2.0},{"id":32,"cupSize":"D","age":22,"money":2.0},{"id":33,"cupSize":"D","age":22,"money":2.0},{"id":34,"cupSize":"D","age":22,"money":2.0},{"id":35,"cupSize":"D","age":22,"money":2.0},{"id":36,"cupSize":"E","age":25,"money":23.0},{"id":37,"cupSize":"E","age":25,"money":23.0},{"id":38,"cupSize":"E","age":25,"money":23.0},{"id":39,"cupSize":"E","age":25,"money":23.0},{"id":40,"cupSize":"E","age":21,"money":23.0},{"id":41,"cupSize":"E","age":21,"money":23.0},{"id":42,"cupSize":"E","age":21,"money":23.0},{"id":43,"cupSize":"E","age":21,"money":23.0},{"id":44,"cupSize":"E","age":21,"money":23.0},{"id":45,"cupSize":"E","age":21,"money":25.0},{"id":46,"cupSize":"E","age":27,"money":25.0})
+
         mockMvc.perform(MockMvcRequestBuilders.get("/girls"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string("200"));
+                .andExpect(MockMvcResultMatchers.status().isOk());
+                //.andExpect(MockMvcResultMatchers.content().string("200"));
     }
+
+
 
 }
